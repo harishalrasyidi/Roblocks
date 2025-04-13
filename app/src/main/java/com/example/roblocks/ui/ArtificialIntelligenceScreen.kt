119a8f9573b800d6cc1499d7196e61910d2756bb
ridhofr <139183192+ridhofrd@users.noreply.github.com>
2025-04-13 14:44:17 +0700

    Create Project + List Project (#7)
    * Room Init, Robotics Screen, AI Screen
    
    * Full Functional BotNavBar, Alert Buat Project
    
    * Buat Proyek
    
    ---------
    
    Co-authored-by: Muhammad Harish Al-Rasyidi <100755379+harishalrasyidi@users.noreply.github.com>

--- app/src/main/java/com/example/roblocks/ui/ArtificialIntelligenceScreen.kt
+++ app/src/main/java/com/example/roblocks/ui/ArtificialIntelligenceScreen.kt
@@ -58,7 +58,7 @@ fun ArtificialIntelligenceScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(20.dp))
 
             //tambah kondisi jadi ada list project waktu DAO udah beres
-            ProjectCard("Project AI", false)
+            ProjectCard("Project AI", false, "AI", navController = navController)
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -69,4 +69,60 @@ fun ArtificialIntelligenceScreen(navController: NavController) {
             )
         }
     }
+}
+
+@Composable
+fun CreateProjectDialogAI(onDismiss: () -> Unit, navController: NavController) {
+    val namaProyek = remember { mutableStateOf("") }
+
+    AlertDialog(
+        containerColor = Color(0xFF4A65FE),
+        onDismissRequest = onDismiss,
+        title = {
+            Text(
+                text = "Proyek AI Baru",
+                style = MaterialTheme.typography.titleMedium.copy(
+                    color = Color.White,
+                    fontSize = 18.sp,
+                ),
+                fontWeight = FontWeight.Bold
+            )
+        },
+        text = {
+            Column {
+                Card(
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFECD46)),
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text(
+                            text = "Pilih Jenis Proyek",
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                color = Color.White,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold
+                            ),
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+
+                        cardJenisProyek("Klasifikasi Gambar", R.drawable.ic_ai, "Buatlah suatu model yang bisa tahu maksud dari  gambar yang kamu kirim!", "ml_image", navController = navController)
+                    }
+                }
+            }
+        },
+        confirmButton = {
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel",
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                    )
+                )
+            }
+        }
+    )
 }
--- app/src/main/java/com/example/roblocks/ui/CreateProjectDialog.kt
+++ app/src/main/java/com/example/roblocks/ui/CreateProjectDialog.kt
@@ -1,6 +1,7 @@
 package com.example.roblocks.ui
 
 import android.graphics.Paint.Align
+import android.graphics.drawable.Icon
 import android.provider.CalendarContract.Colors
 import android.renderscript.ScriptGroup.Input
 import android.view.RoundedCorner
@@ -28,16 +29,21 @@ import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
 import com.example.roblocks.R
 
-
 @Composable
-fun CreateProjectDialog(onDismiss: () -> Unit) {
+fun insertNamaProyek(
+    title: String,
+    route: String,
+    onDismiss: () -> Unit,
+    navController: NavController
+){
     val namaProyek = remember { mutableStateOf("") }
 
     AlertDialog(
+        containerColor = Color(0xFFA199FF),
         onDismissRequest = onDismiss,
         title = {
             Text(
-                text = "Proyek AI Baru",
+                text = title,
                 style = MaterialTheme.typography.titleMedium.copy(
                     color = Color.White,
                     fontSize = 18.sp,
@@ -48,25 +54,36 @@ fun CreateProjectDialog(onDismiss: () -> Unit) {
         text = {
             Column {
                 Card(
-                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFECD46)),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFF55A8)),
                     shape = RoundedCornerShape(8.dp),
-                    modifier = Modifier.padding(10.dp)
+                    modifier = Modifier.padding(10.dp).fillMaxWidth().wrapContentHeight()
                 ) {
                     Column(modifier = Modifier.padding(16.dp)) {
                         Text(
                             text = "Nama Proyek",
                             style = MaterialTheme.typography.titleMedium.copy(
                                 color = Color.White,
-                                fontSize = 12.sp,
+                                fontSize = 16.sp,
                             ),
                             fontWeight = FontWeight.Bold
                         )
                         Spacer(modifier = Modifier.height(4.dp))
                         TextField(
                             value = namaProyek.value,
+                            textStyle = MaterialTheme.typography.titleMedium.copy(
+                                color = Color.Black,
+                                fontSize = 16.sp,
+                            ),
                             onValueChange = { namaProyek.value = it },
                             modifier = Modifier.fillMaxWidth(),
-                            label = { Text("Masukkan Nama Proyek") }
+                            label = { Text("Masukkan Nama Proyek") },
+                            colors = TextFieldDefaults.colors(
+                                unfocusedContainerColor = Color.White,
+                                focusedContainerColor = Color.White,
+                                focusedIndicatorColor = Color.Magenta,
+                                unfocusedIndicatorColor = Color.Magenta,
+                            ),
+                            shape = RoundedCornerShape(8.dp),
                         )
                     }
                 }
@@ -74,16 +91,147 @@ fun CreateProjectDialog(onDismiss: () -> Unit) {
         },
         confirmButton = {
             TextButton(onClick = {
-                // Handle the confirm button click, like saving the project
-                onDismiss() // Close the dialog after confirmation
+                onDismiss()
+                navController.navigate(route)
             }) {
-                Text("Buat Proyek")
+                Text(
+                    "Buat Proyek",
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                    )
+                )
             }
         },
         dismissButton = {
             TextButton(onClick = onDismiss) {
-                Text("Cancel")
+                Text(
+                    "Cancel",
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                    )
+                )
             }
         }
     )
 }
+
+@Composable
+fun cardJenisProyek(
+    title: String,
+    icon: Int,
+    description: String,
+    route: String,
+    navController: NavController
+) {
+    var inputNamaProyek by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFA199FF),
+            contentColor = Color.White
+        ),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 12.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                // LEFT: Title + Description + Button
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleMedium.copy(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = description,
+                        style = MaterialTheme.typography.bodySmall.copy(
+                            fontSize = 12.sp
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(16.dp)),
+                        tint = Color.White,
+                    )
+
+                }
+            }
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp, vertical = 2.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                    Button(
+                        onClick = { /* TODO */ },
+                        colors = ButtonDefaults.buttonColors(Color(0xFFFF55A8)),
+                        shape = RoundedCornerShape(50),
+                        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "Lihat Contoh!",
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    }
+
+                    Spacer(Modifier.weight(0.3f))
+
+                    Button(
+                        onClick = { inputNamaProyek = true },
+                        colors = ButtonDefaults.buttonColors(Color(0xFF2FCEEA)),
+                        shape = RoundedCornerShape(50),
+                        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "Buat Proyek",
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+
+                    }
+                if(inputNamaProyek == true){
+                    insertNamaProyek(title, route, onDismiss = {inputNamaProyek = false}, navController)
+                }
+            }
+        }
+    }
+}
+
